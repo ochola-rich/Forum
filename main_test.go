317470fd8eb66f6c7d4c48a9a780a63a9ce1a87e
Geremi57 <wangageremi725@gmail.com>
2026-04-15 17:59:12 +0300

    add simple test for register endpoint

--- main_test.go
+++ main_test.go
@@ -37,10 +37,27 @@ func TestPing(t *testing.T){
 	if desiredcode != w.Code {
 		t.Errorf("Bad response expected: %v but received: %v", desiredcode, w.Code)
 	}
-	
 	expectedMessage := []byte("Hello jack!\n")
 
 	if !bytes.Equal(expectedMessage, w.Body.Bytes()) {
 		t.Errorf("Bad response body expected: %q but received: %v", expectedMessage, w.Body.String())
+	}
+}
+
+func TestRegister(t *testing.T){
+	req := httptest.NewRequest(http.MethodGet, "/register?user=jack", nil)
+
+	desiredcode := http.StatusOK
+
+	w := httptest.NewRecorder()
+	register(w, req)
+	if w.Code != desiredcode {
+		t.Errorf("bad response expected: %v but received: %v body %v", desiredcode, w.Code, w.Body.String())
+	}
+
+	expectedMessage := []byte("Welcome jack!\n")
+
+	if !bytes.Equal(expectedMessage, w.Body.Bytes()) {
+		t.Errorf("bad body expected: %q but receives: %q", expectedMessage, w.Body.String())
 	}
 }
